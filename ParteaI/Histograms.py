ddbdcee640a59399c56aa163c52029ff8c6eaea0
MateyPit <nicolescupetrisor@gmail.com>
2024-05-27 19:06:47 +0300

    Finished third task

---
+++ ParteaI/Histograms.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+df = pd.read_csv('train.csv')
+
+ages = df["Age"]
+
+age_hist = plt.figure()
+plt.hist(ages)
+plt.title("Age Distribution")
+plt.xlabel("Ages")
+plt.ylabel("Number of Passengers")
+age_hist.savefig("AgeHistogram")
+
+fares = df["Fare"]
+
+fare_hist = plt.figure()
+plt.hist(fares)
+plt.title("Fares paid by passengers")
+plt.xlabel("Fares")
+plt.ylabel("Number of Passengers")
+fare_hist.savefig("FareHistogram")
+
+sibsp = df["SibSp"]
+
+sibsp_hist = plt.figure()
+plt.hist(sibsp)
+plt.title("Siblings/Spouses for each passenger")
+plt.xlabel("Number of siblings and spouses")
+plt.ylabel("Number of Passengers")
+sibsp_hist.savefig("SibSpHistogram")
+
+parch = df["Parch"]
+
+parch_hist = plt.figure()
+plt.hist(parch)
+plt.title("Parents/Children for each passenger")
+plt.xlabel("Number of parents and children")
+plt.ylabel("Number of Passengers")
+plt.xlim(0, 5.5)
+parch_hist.savefig("ParchHistogram")
