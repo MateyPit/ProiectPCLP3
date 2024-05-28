50260d74445d59a43de5908da91eea111e009b91
MateyPit <nicolescupetrisor@gmail.com>
2024-05-28 16:26:59 +0300

    Finished the eighth task

---
+++ ParteaI/FillData.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+df = pd.read_csv('train.csv')
+
+lines = len(df)
+
+missing_data_cols = df.isnull().sum()
+
+cols = missing_data_cols[missing_data_cols > 0]
+
+list_cols = list(cols.index)
+
+df.loc[df['Survived'] == 1, 'Age'].mean()
+
+for i in range(0, len(list_cols)):
+    try:
+        mean_value = round(df.loc[df['Survived'] == 1, list_cols[i]].mean())
+        print(f'Average value for "{list_cols[i]}": {mean_value}')
+    except TypeError:
+        mean_value = (df.loc[df['Survived'] == 1, list_cols[i]]).mode()[0]
+        print(f'Most common value for "{list_cols[i]}": {mean_value}')
+    df[list_cols[i]] = df[list_cols[i]].fillna(mean_value)
