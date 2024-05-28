53114b57b53986661aaecbf8fcfe9cff48765bb2
MateyPit <nicolescupetrisor@gmail.com>
2024-05-28 21:27:50 +0300

    Added files for tenth task

---
+++ ParteaI/Task10_RelationData.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+
+df = pd.read_csv('train.csv')
+
+df['Family members'] = df['SibSp'] + df['Parch']
+
+family_hist = plt.figure()
+sns.histplot(data = df, x = 'Family members', hue = 'Survived', palette = 'bright', stat = 'percent')
+plt.title("Survival rate based on family size")
+family_hist.savefig("Task10_FamilyGraph")
+
+df = pd.read_csv('train.csv')
+
+relation_graph = sns.catplot(data = df.head(100), x = 'Pclass', y = 'Fare', hue = 'Survived', kind = 'swarm')
+plt.title("Relationship between paid fare, class and rate of survival")
+relation_graph.savefig("Task10_RelationGraph")
