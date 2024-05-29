6179847d9a5179910127e7c471541202a57cdf12
MateyPit <nicolescupetrisor@gmail.com>
2024-05-29 18:44:01 +0300

    Updated fourth task to use sum()

--- ParteaI/Surse/Task4_MissingData.py
+++ ParteaI/Surse/Task4_MissingData.py
@@ -14,15 +14,9 @@ list_cols = list(cols.index)
 
 for i in range(0, len(list_cols)):
     print(f'Percent of missing data for column "{list_cols[i]}": {round(cols.iloc[i] / lines * 100, 2)}%')
-    try:
-        survival_stats = df.loc[df['Survived'] == 1, list_cols[i]].isnull().value_counts()[True]
-    except KeyError:
-        survival_stats = 0
+    survival_stats = df.loc[df['Survived'] == 1, list_cols[i]].isnull().sum()
     print(f'People with missing data in column "{list_cols[i]}" who have survived: {round(survival_stats / cols.iloc[i] * 100, 2)}%')
-    try:
-        deceased_stats = df.loc[df['Survived'] == 0, list_cols[i]].isnull().value_counts()[True]
-    except KeyError:
-        deceased_stats = 0
+    deceased_stats = df.loc[df['Survived'] == 0, list_cols[i]].isnull().sum()
     print(f'People with missing data in column "{list_cols[i]}" who have died: {round(deceased_stats / cols.iloc[i] * 100, 2)}%')
     print()
 
